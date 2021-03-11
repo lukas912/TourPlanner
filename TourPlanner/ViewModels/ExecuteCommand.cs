6061598ba6b115598088b65ef9da5de2de635f13
Lukas Nowy <lukas.n912@gmail.com>
2021-03-11 13:50:35 +0100

    Execute Command implemented

---
+++ TourPlanner/ViewModels/ExecuteCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+
+namespace TourPlanner.ViewModels
+{
+    class ExecuteCommand : ICommand
+    {
+        private readonly MainViewModel _mainViewModel;
+
+        public ExecuteCommand(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+            _mainViewModel.PropertyChanged += (sender, args) =>
+            {
+                Debug.Print("command: reveived prop changed");
+                if (args.PropertyName == "Input")
+                {
+                    Debug.Print("command: reveived prop changed of Input");
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            Debug.Print("command: can execute");
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Debug.Print("command: execute");
+            _mainViewModel.Output = "Command executed!!";
+            Debug.Print("command: execute done");
+        }
+    }
+}
--- TourPlanner/ViewModels/MainViewModel.cs
+++ TourPlanner/ViewModels/MainViewModel.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
 
 namespace TourPlanner.ViewModels
 {
     class MainViewModel : INotifyPropertyChanged
     {
+        private string _output = "Not clicked";
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand ExecuteCommand { get; }
+
+        public MainViewModel()
+        {
+            Debug.Print("ctor MainViewModel");
+            this.ExecuteCommand = new ExecuteCommand(this);
+        }
+
+        public string Output
+        {
+            get
+            {
+                Debug.Print("read Output");
+                return _output;
+            }
+            set
+            {
+                Debug.Print("write Output");
+                Debug.Print("set Output");
+                _output= value;
+                Debug.Print("fire propertyChanged: Output");
+                OnPropertyChanged();
+
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.Print($"propertyChanged \"{propertyName}\"");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
